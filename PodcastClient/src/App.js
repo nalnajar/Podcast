396b4340336167bb39fb1998a5d31f8037de8269
Ralph Constantin <ralph.constantin@hotmail.com>
2023-10-31 16:10:32 -0400

    added contact us page and modified the about page

--- PodcastClient/src/App.js
+++ PodcastClient/src/App.js
@@ -4,6 +4,7 @@ import LoginSignup from "./Components/LoginSignupComponent/LoginSignup";
 import Home from "./Components/Home/Home";
 import AboutPage from "./Components/About/AboutPage";
 import { Footer } from "./Components/Footer/Footer";
+import ContactUs from "./Components/ContactUs/ContactUs";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/Home" element={<Home />} />
         <Route path="/About" element={<AboutPage />} />
+        <Route path="/Contact-us" element={<ContactUs/>} />
         <Route path="/Signup" element={<LoginSignup />} />
         <Route path="/Log-in" element={<LoginSignup />} />
       </Routes>
--- PodcastClient/src/Components/About/AboutPage.jsx
+++ PodcastClient/src/Components/About/AboutPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import SoundBarrierLogo from "../Assets/SoundBarrierLogo.png";
 import Waves from "../Assets/Waves.png";
 import { HeaderBar } from "../Common/Common";
+import "./AboutPage.css";
 
 const AboutPage = () => {
   const coders = [
@@ -24,26 +25,26 @@ const AboutPage = () => {
   return (
     <div>
       <HeaderBar />
-      <div style={styles.container}>
-        <h1 style={styles.title}>About Us</h1>
-        <img src={SoundBarrierLogo} alt="Podcast Logo" style={styles.logo} />
+      <div className="about-container">
+        <h1 className="about-title">About Us</h1>
+        <img src={SoundBarrierLogo} alt="Podcast Logo" className="about-logo" />
         <br />
-        <div style={styles.description}>
-          <div style={styles.row1}>
-            <div style={styles.column}>
-              <h2>Discover and Share Unique Voices</h2>
+        <div className="about-description">
+          <div className="about-row1">
+            <div className="about-column">
+              <h2>Discover Voices</h2>
               Sound Barrier isn't just another social media platform; it's a
               revolution in the world of podcasts. Dive deep into a vast library
               of voices and stories waiting to be heard.
             </div>
-            <div style={styles.column}>
+            <div className="about-column">
               <h2>For the Creators</h2>
               Unleash your voice! Upload your audio podcast and start reaching
               listeners in seconds. We believe in giving creators the freedom
               they deserve, which means fewer hoops to jump through and more
               voices to be heard.
             </div>
-            <div style={styles.column}>
+            <div className="about-column">
               <h2>For the Listeners</h2>
               Find new favorites. Explore a world of content uploaded by users
               just like you. Whether you're into storytelling, news, or
@@ -51,8 +52,8 @@ const AboutPage = () => {
               you.
             </div>
           </div>
-          <div style={styles.row2}>
-            <div style={styles.twoColumn}>
+          <div className="about-row2">
+            <div className="about-twoColumn">
               <h2>Safety and Community First</h2>
               While we pride ourselves on freedom, we also take safety
               seriously. We review user-reported content and ensure that
@@ -60,7 +61,7 @@ const AboutPage = () => {
               Barrier's mission is to create an environment that's both
               liberating for creators and safe for its community.
             </div>
-            <div style={styles.twoColumn}>
+            <div className="about-twoColumn">
               <h2>Why Choose Us</h2>
               We're not just another podcast platform; we're a community. With
               an interface designed for ease of use, unmatched features, and a
@@ -71,13 +72,13 @@ const AboutPage = () => {
         <br />
         <br />
         {wavesArray.map((_, index) => (
-          <img key={index} src={Waves} alt="Waves" style={styles.waves} />
+          <img key={index} src={Waves} alt="Waves" className="about-waves" />
         ))}
         <br />
         <h2>Our Coders</h2>
-        <ul style={styles.codersList}>
+        <ul className="about-codersList">
           {coders.map((coder) => (
-            <li key={coder} style={styles.coderName}>
+            <li key={coder} className="about-coderName">
               {coder}
             </li>
           ))}
@@ -85,64 +86,6 @@ const AboutPage = () => {
       </div>
     </div>
   );
-};
-
-const styles = {
-  container: {
-    fontFamily: "Arial, sans-serif",
-    margin: "2%",
-    textAlign: "center",
-  },
-  title: {
-    color: "#333",
-    fontSize: "2em",
-  },
-  logo: {
-    width: "250px",
-    height: "auto",
-  },
-  codersList: {
-    listStyleType: "none",
-    padding: 0,
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "center",
-  },
-  coderName: {
-    width: "26%",
-    boxSizing: "border-box",
-    padding: "10px",
-    fontSize: "1.2em",
-    textAlign: "center",
-  },
-  row1: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    marginBottom: "1%",
-  },
-  row2: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    marginBottom: "1.5rem",
-    marginLeft: "5%",
-    marginRight: "5%",
-  },
-  column: {
-    marginLeft: "2%",
-    marginRight: "2%",
-  },
-  twoColumn: {
-    marginTop: "1%",
-    marginLeft: "5%",
-    marginRight: "5%",
-  },
-  description: {
-    marginLeft: "25%",
-    marginRight: "25%",
-  },
-  waves: { width: "100px" },
 };
 
 export default AboutPage;
