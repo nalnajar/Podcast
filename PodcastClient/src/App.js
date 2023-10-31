c65ed3c4b367234db8f0bbf78e75cf48e86687c5
slinkyGirrage <idrikare663@gmail.com>
2023-10-31 16:46:11 -0400

    uploading the help age

--- PodcastClient/src/App.js
+++ PodcastClient/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginSignup from './Components/LoginSignupComponent/LoginSignup';
 import DeveloperPage from './Components/DevelopersPageComponent/developerspage';
+import HelpPage from './Components/HelpPage/HelpPage';
 
 function App() {
   return (
     <Router>
       <div>
         <Routes>
+        <Route path="/HelpPage" element={<HelpPage />} />
           <Route path="/DeveloperPage" element={<DeveloperPage />} />
           <Route path="/" element={<LoginSignup />} />
         </Routes>
---
+++ PodcastClient/src/Components/HelpPage/HelpPage.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css'; // Import the CSS file with your styles
+
+
+function FAQQuestion(props) {
+  const [isOpen, setIsOpen] = useState(false);
+  
+  return (
+    <div className={`faq-question ${isOpen ? 'faqQuestionOpen' : ''}`}>
+      <div className="questionHeader" onClick={() => setIsOpen(!isOpen)}>
+        <h3 className="faqQuestionH3">{props.question}</h3>
+      </div>
+      <p className={`answer ${isOpen ? 'faqQuestionOpenAnswer' : ''}`}>{props.answer}</p>
+    </div>
+  );
+}
+
+function HelpPage() {
+  const [input, setInput] = useState("");
+  const [results, setResults] = useState([]);
+
+  function handleChange(event) {
+    setInput(event.target.value);
+    searchFAQs(event.target.value);
+  }
+
+  function searchFAQs(query) {
+    // Simulated web search results
+    const webResults = [
+      {
+        title: "How do I change my profile pic?",
+        snippet: "You can change your profile pic by clicking on your avatar in the top right corner and selecting 'Edit Profile'. Then, you can upload a new picture or choose from our gallery.",
+      },
+      {
+        title: "Where is my cheese?",
+        snippet: "Your cheese is in the fridge unless someone ate it. If you can't find it, please contact our support team, and we will send you more cheese.",
+      },
+      {
+        title: "How can we assist you today?",
+        snippet: "We are here to help you with any questions or issues you may have. You can browse our FAQs, contact our support team, or visit our other pages for more information.",
+      },
+    ];
+
+    setResults(webResults);
+  }
+
+  return (
+    <div className="help-container"> {/* Use the CSS class name */}
+      <header className="header">
+        <h1>Help Page</h1>
+        <button className="menuButton">Menu</button>
+      </header>
+      <section className="faqSection">
+        <h2>Frequently Asked Questions</h2>
+        <p>How can we assist you today?</p>
+        <input
+          type="text"
+          value={input}
+          onChange={handleChange}
+          placeholder="Search FAQs"
+        />
+        {results.length > 0 && (
+          <div className="searchResults">
+            {results.map((result, index) => (
+              <FAQQuestion
+                key={index}
+                question={result.title}
+                answer={result.snippet}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+      <div className="dropdown-list">
+
+    </div>
+      <section className="otherSection">
+        <h2>Other Pages</h2>
+        <a className="otherSectionA" href="/contact">Contact Support</a>
+        <a className="otherSectionA" href="/links">Links Here</a>
+      </section>
+    </div>
+  );
+}
+
+ReactDOM.render(<HelpPage />, document.getElementById('root'));
+
+export default HelpPage;
