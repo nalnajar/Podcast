e06a3f8abe95a61b8455f34ab0c5bf3a79852adc
Gavin Guerette <gavin.guerette@hotmail.com>
2023-12-05 18:18:18 -0500

    Example applied

--- PodcastClient/src/Components/Home/Home.jsx
+++ PodcastClient/src/Components/Home/Home.jsx
@@ -5,7 +5,16 @@ const Home = () => {
   return (
     <div>
       <HeaderBar />
-      <PodcastData />
+      <PodcastData
+        name={"podcast name"}
+        artist={"podcast artist"}
+        collection={"this is part of a collection"}
+      />
+      <PodcastData
+        name={"another podcast name"}
+        artist={"another podcast artist"}
+        collection={""}
+      />
     </div>
   );
 };
