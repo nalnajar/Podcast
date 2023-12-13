6d9967613e1fd69c3f7e0c4fc77b3440d8d70cd2
slinkyGirrage <idrikare663@gmail.com>
2023-12-12 20:43:27 -0500

    dummy data/placeholders num2 for the profileviewer

--- PodcastClient/src/Components/ProfilePage/ProfileViewer.jsx
+++ PodcastClient/src/Components/ProfilePage/ProfileViewer.jsx
@@ -1,40 +1,42 @@
-import React, { useState, useEffect, Fragment } from 'react'
-import { Link } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { HeaderBar } from "../Common/Common";
-
+import React, { useState, useEffect, Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import { HeaderBar } from '../Common/Common';
+import axios from 'axios';
 
 const UserPage = ({ userId }) => {
   const [userData, setUserData] = useState(null);
-  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
 
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        // Replace 'YOUR_API_ENDPOINT' with your actual API endpoint
+        const response = await axios.get(`YOUR_API_ENDPOINT/users/${userId}`);
+        setUserData(response.data);
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    };
 
+    fetchUserData();
+  }, [userId]);
 
   return (
     <div>
       <HeaderBar />
-      {isAuthenticated ? (
+      {userData ? (
         <Fragment>
-          {userData ? (
-            <div>
-                
-              {/* This is just dummy data to go off of */}
-             <img src={userData.profilePicture} alt="Profile" className='profile-picture' />
-              <h3>{userData.username}</h3>
-              <p><strong>Followers:</strong> {userData.totalFollowers}</p>
-              <p><strong>Total Podcasts:</strong> {userData.totalPodcasts}</p>
+          <div>
+            {/* This is just dummy data to go off of */}
+            <img src={userData.profilePicture} alt="Profile" className='profile-picture' />
+            <h3>{userData.username}</h3>
+            <p><strong>Followers:</strong> {userData.totalFollowers}</p>
+            <p><strong>Total Podcasts:</strong> {userData.totalPodcasts}</p>
 
-              {/* Display other user data as needed */}
-
-            </div>
-          ) : (
-            <p>Loading user data...</p>
-          )}
+            {/* Display other user data as needed */}
+          </div>
         </Fragment>
       ) : (
-        <p>
-          Please <Link to="/login">log in</Link> to view this page.
-        </p>
+        <p>Loading user data...</p>
       )}
     </div>
   );
