6a0b84213302bd9e514ec8cb96f817c0f4fe0fcd
Ralph Constantin <ralph.constantin@hotmail.com>
2023-12-12 21:11:39 -0500

    added placeholder profile pic and some stuff

--- PodcastClient/src/Components/ProfilePage/ProfileViewer.jsx
+++ PodcastClient/src/Components/ProfilePage/ProfileViewer.jsx
@@ -1,43 +1,44 @@
 import React, { useState, useEffect, Fragment } from "react";
-import { Link } from "react-router-dom";
 import { HeaderBar } from "../Common/Common";
 import axios from "axios";
+import ProfilePicture from "../Assets/profilepic_placeholder.jpg";
+import "./ProfileViewer.css";
 
 const UserPage = ({ userId }) => {
-  const [userData, setUserData] = useState(null);
+  //   const [userData, setUserData] = useState(null);
+  const username = localStorage.getItem("username");
 
-  useEffect(() => {
-    const fetchUserData = async () => {
-      try {
-        // Replace 'YOUR_API_ENDPOINT' with your actual API endpoint
-        const response = await axios.get(`YOUR_API_ENDPOINT/users/${userId}`);
-        setUserData(response.data);
-      } catch (error) {
-        console.error("Error fetching user data:", error);
-      }
-    };
+  //   useEffect(() => {
+  // const fetchUserData = async () => {
+  //   try {
+  //     // Replace 'YOUR_API_ENDPOINT' with your actual API endpoint
+  //     const response = await axios.get(
+  //       `http://localhost:8081/users/${userId}`
+  //     );
+  //     setUserData(response.data);
+  //   } catch (error) {
+  //     console.error("Error fetching user data:", error);
+  //   }
+  // };
 
-    fetchUserData();
-  }, [userId]);
+  // fetchUserData();
+  //   }, [userId]);
 
   return (
     <div>
       <HeaderBar />
-      {userData ? (
+      {username !== "" ? (
         <Fragment>
           <div>
             {/* This is just dummy data to go off of */}
             <img
-              src={userData.profilePicture}
+              src={ProfilePicture}
               alt="Profile"
               className="profile-picture"
             />
-            <h3>{userData.username}</h3>
+            <h2 className="username-header">{username}</h2>
             <p>
-              <strong>Followers:</strong> {userData.totalFollowers}
-            </p>
-            <p>
-              <strong>Total Podcasts:</strong> {userData.totalPodcasts}
+              <strong>Total Podcasts:</strong>
             </p>
 
             {/* Display other user data as needed */}
